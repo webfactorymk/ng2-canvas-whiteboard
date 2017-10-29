a2b10cadf46fcdcf4765103c42c54bc3c62dc826
abdosaeedelhassan <abdosaeedelhassan@gmail.com>
2017-10-29 20:30:01 +0300

    sdfghjkl;

--- dist/ng2-canvas-whiteboard.d.ts
+++
@@ -1,4 +0,0 @@
-export { CanvasWhiteboardComponent } from "./canvas-whiteboard.component";
-export { CanvasWhiteboardUpdate } from "./canvas-whiteboard-update.model";
-export declare class CanvasWhiteboardModule {
-}
--- dist/template.d.ts
+++
@@ -1,2 +0,0 @@
-export declare const DEFAULT_TEMPLATE: string;
-export declare const DEFAULT_STYLES: string;
