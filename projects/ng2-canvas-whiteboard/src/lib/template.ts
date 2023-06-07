d8bc4f77b46b1747d5f5d9d03534c9c0486f611e
Goran Lilcikj <goran.lilcikj@webfactory.mk>
2023-06-07 16:09:05 +0200

    Fix build errors

--- projects/ng2-canvas-whiteboard/src/lib/template.ts
+++ projects/ng2-canvas-whiteboard/src/lib/template.ts
@@ -29,7 +29,6 @@ export const DEFAULT_STYLES = `
 @media (max-width: 400px) {
      .canvas_whiteboard_buttons {
             position: absolute;
-            z-inde
             top: 0;
             width: 100%;
             text-align: center;
